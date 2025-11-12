4f19f10b672c858d27b03fc2c3aa342726e277c0
Hugo Moraga Morales <hmoragam@entel.cl>
2025-11-11 23:34:04 -0300

    Add .gitignore file to admin directory to exclude index.html and assets folder

--- tina/config.ts
+++ tina/config.ts
@@ -17,28 +17,169 @@ export default defineConfig({
 
   build: {
     outputFolder: "admin",
-    publicFolder: "public",
+    publicFolder: "./",
   },
   media: {
     tina: {
-      mediaRoot: "",
-      publicFolder: "public",
+      mediaRoot: "assets/images",
+      publicFolder: "./",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/r/content-modelling-collections/
   schema: {
     collections: [
       {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
+        name: "diario",
+        label: "✶ Diario",
+        path: "_posts",
+        format: "md",
         fields: [
+          {
+            type: "string",
+            name: "layout",
+            label: "Layout",
+            default: "default",
+          },
           {
             type: "string",
             name: "title",
             label: "Title",
             isTitle: true,
             required: true,
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Image",
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Publish Date",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "poemas",
+        label: "ↂ Poemas",
+        path: "_poems",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "layout",
+            label: "Layout",
+            default: "poems",
+            options: ["poems", "default"],
+          },
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Image",
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+          },
+          {
+            type: "string",
+            name: "series",
+            label: "Series",
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "vestigios",
+        label: "⚱ Vestigios",
+        path: "_vestigios",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "layout",
+            label: "Layout",
+            default: "default",
+          },
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Image",
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "imagenes",
+        label: "◉ Imágenes",
+        path: "_images",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "layout",
+            label: "Layout",
+            default: "imagen",
+          },
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Image",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "caption",
+            label: "Caption",
+          },
+          {
+            type: "string",
+            name: "alt",
+            label: "Alt Text",
           },
           {
             type: "rich-text",
